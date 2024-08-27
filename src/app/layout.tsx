68e2baa5b73ddaec30ede17042702339f78d6dd9
Adonis <adonis.casado@outlook.com>
2024-08-27 17:40:08 -0400

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter, Calistoga } from 'next/font/google';
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ 
+  subsets: ['latin'], 
+  variable: '--font-sans' 
+});
+
+const calistoga = Calistoga({ 
+  subsets: ['latin'], 
+  variable: '--font-serif', 
+  weight: ['400'] 
+});
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+  description: "Created with the help of Frontend Tribe",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={twMerge(inter.className, calistoga.className, 
+      "bg-gray-100 text-gray-800 antialiased")}>{children}</body>
+    </html>
+  );
+}
